799eb1fcb89303eae3927862aae60d138d7da440
carzal <lmcaroz@unal.edu.co>
2023-10-20 20:36:46 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,11 +11,21 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
+import re
+
+
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_fwf("clusters_report.txt", skiprows=4, names = ['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave'])
+    df.fillna(method='ffill',inplace=True)
+    df.principales_palabras_clave = df.principales_palabras_clave.str.strip()
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace(" %","",regex=False).str.replace(",",".",regex=False).astype('float')
+    df = df.groupby(['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave'])['principales_palabras_clave'].apply(lambda x : ','.join(x.str.strip())).reset_index()
+    df.principales_palabras_clave = df.principales_palabras_clave.str.replace(',,',", ")
+    df.principales_palabras_clave = df.principales_palabras_clave.apply(lambda x: re.sub(r'(\w),(\w)', r'\1 \2', x))
+    df.principales_palabras_clave = df.principales_palabras_clave.str.replace('  '," ").str.replace('   '," ").str.replace('    '," ")
+    df.principales_palabras_clave = df.principales_palabras_clave.str.replace('  '," ")
+    df.principales_palabras_clave = df.principales_palabras_clave.str.replace('.',"",regex=True)
 
     return df
